67ed903bbf31dcc77c405c11ba84d6d8c1001010
macLC <772812969@qq.com>
2018-03-10 20:19:00 +0800

    With navigation bar

--- NursingHomeManagementSystem/src/App.vue
+++ NursingHomeManagementSystem/src/App.vue
@@ -1,29 +1,59 @@
 <template>
   <div id="app">
-  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-    
-    <el-menu-item index="1">处理中心</el-menu-item>
-    <el-submenu index="2">
-      <template slot="title">我的工作台</template>
-      <el-menu-item index="2-1">选项1</el-menu-item>
-      <el-menu-item index="2-2">选项2</el-menu-item>
-      <el-menu-item index="2-3">选项3</el-menu-item>
-      <el-submenu index="2-4">
-        <template slot="title">选项4</template>
-        <el-menu-item index="2-4-1">选项1</el-menu-item>
-        <el-menu-item index="2-4-2">选项2</el-menu-item>
-        <el-menu-item index="2-4-3">选项3</el-menu-item>
-      </el-submenu>
-    </el-submenu>
-    <el-menu-item index="3" disabled>消息中心</el-menu-item>
-    <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-  </el-menu>
-  <div class="line"></div>
+    <el-container>
+      <el-header id="header">陈娟英敬老院</el-header>
+      <el-row class="tac">
+        <el-container>
+          <el-aside :span="8">
+            <el-menu
+              default-active="2"
+              class="el-menu-vertical-demo"
+              @open="handleOpen"
+              @close="handleClose">
+              <el-submenu index="1">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>导航一</span>
+                </template>
+                <el-menu-item-group>
+                  <template slot="title">分组一</template>
+                  <el-menu-item index="1-1">选项1</el-menu-item>
+                  <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                  <el-menu-item index="1-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                  <template slot="title">选项4</template>
+                  <el-menu-item index="1-4-1">选项1</el-menu-item>
+                </el-submenu>
+              </el-submenu>
+              <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">导航二</span>
+              </el-menu-item>
+              <el-menu-item index="3" disabled>
+                <i class="el-icon-document"></i>
+                <span slot="title">导航三</span>
+              </el-menu-item>
+              <el-menu-item index="4">
+                <i class="el-icon-setting"></i>
+                <span slot="title">导航四</span>
+              </el-menu-item>
+            </el-menu>            
+          </el-aside>
+          <el-main :span="16">Main</el-main>
+        </el-container>
+      </el-row>
+    </el-container>    
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    
+  },
   methods: {
     handleSelect(key, keyPath) {
       console.log(key, keyPath);
@@ -37,4 +67,23 @@ export default {
   font-family: Helvetica, sans-serif;
   text-align: center;
 }
+
+#header {
+  color: #616161;
+  font-size: 24px;
+  font-family: Helvetica Neue, sans-serif;
+  font-weight: 900;
+  text-align: left;
+}
+
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+
 </style>
